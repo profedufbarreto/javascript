9f17e5f9600f8420c918b8f12f7993fa417bd78b
profedufbarreto <01289211027@senacrs.edu.br>
2026-02-07 16:22:58 -0300

    funções com switch

--- index.js
+++ index.js
@@ -2,6 +2,39 @@ function soma(a, b){
     return a + b;
 }
 
-resultado = soma(4, 5);
+function subt(a, b){
+    return a - b;
+}
 
-console.log(resultado);
+function mult(a, b){
+    return a * b;
+}
+
+function div(a, b){
+    return a / b;
+}
+
+opcao = 3;
+
+switch(opcao){
+    case 1:
+        resultado = soma(5, 5);
+        console.log(`O resultado é ${resultado}.`);
+        break;
+    case 2:
+        resultado = subt(5, 5);
+        console.log(`O resultado é ${resultado}.`);
+        break;
+    case 3:
+        resultado = mult(5, 5);
+        console.log(`O resultado é ${resultado}.`);
+        break;
+    case 4:
+        resultado = div(5, 5);
+        console.log(`O resultado é ${resultado}.`);
+        break;
+    default:
+        console.log(`Valor inexistente!`);
+}
+
+
