bddcda63d4546fc9d67a20ef638b7ce5a156de89
profedufbarreto <01289211027@senacrs.edu.br>
2026-02-07 11:57:43 -0300

    for

---
+++ index.js
@@ -0,0 +1,8 @@
+let num = 4;
+let tab = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let resultado;
+
+for(let i = 0; i < 10; i++){
+    resultado = num * tab[i];
+    console.log(`O resultado é ${resultado}.`);
+}
