916a9bd349dea0a40096df756152e2f73711c23d
profedufbarreto <01289211027@senacrs.edu.br>
2026-02-07 12:03:09 -0300

    let a variável que muda

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-let num1 = 4;
-let num2 = 3;
-let nome = "Eduardo";
-let sobrenome = "Barreto";
+let num = 4;
+num = 6;
 
-let nomeCompleto = nome + " " + sobrenome;
-let soma = num1 + num2;
-
-console.log("O resultado da soma é: " + soma);
-console. log("Meu nome é " + nomeCompleto);
+console.log(num);
