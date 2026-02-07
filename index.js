d9be1c2022b57ad5453dbab716c1ea0ced7d4176
profedufbarreto <01289211027@senacrs.edu.br>
2026-02-07 16:02:24 -0300

    function

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-function saudacao(nome){
-    console.log("Olá " + nome + " seja bem-vindo!");
+function soma(a, b){
+    return a + b;
 }
 
-saudacao("Eduardo");
+resultado = soma(4, 5);
+
+console.log(resultado);
