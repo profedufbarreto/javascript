8ec08d528ccbe3817d43a167a536b6ed0d0342eb
profedufbarreto <01289211027@senacrs.edu.br>
2026-02-07 16:50:14 -0300

    for com array

--- index.js
+++ index.js
@@ -1,44 +1,7 @@
-function soma(a, b){
-    return a + b;
-}
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-function subt(a, b){
-    return a - b;
-}
-
-function mult(a, b){
-    return a * b;
-}
-
-function div(a, b){
-    if(b <= 0){
-       return "O dividor não pode ser menor ou igual a 0!";
-    }else{
-        return a / b;
-    }
-}
-
-opcao = 4;
-
-switch(opcao){
-    case 1:
-        resultado = soma(5, 5);
-        console.log(`O resultado é ${resultado}.`);
-        break;
-    case 2:
-        resultado = subt(5, 5);
-        console.log(`O resultado é ${resultado}.`);
-        break;
-    case 3:
-        resultado = mult(5, 5);
-        console.log(`O resultado é ${resultado}.`);
-        break;
-    case 4:
-        resultado = div(5, 5);
-        console.log(`O resultado é ${resultado}.`)
-        break;
-    default:
-        console.log(`Valor inexistente!`);
+for(let i = 0; i <= 10; i++){
+    console.log(`Iterando ${i} vezes.`);
 }
 
 
