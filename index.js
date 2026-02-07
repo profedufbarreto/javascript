39249bff5652c75212519c47ac3a57ff5c9420ae
profedufbarreto <01289211027@senacrs.edu.br>
2026-02-07 14:07:28 -0300

    Complete os outros dias da semana, lembrando que assim valida qualquer entrada feita pelo user

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
-const dia = "domingo";
+const dia = "segunda";
 
 switch (dia){
     case "Segunda":
+    case "segunda":
+    case "SEGUNDA":
         console.log("Segunda-feira");
         break;
+    
     case "Terça":
+    case "terça":
+    case "TERÇA":
         console.log("Terça-feira");
         break;
-    case "Quarta":
-        console.log("Quarta-feira");
-        break;
-    case "Quinta":
-        console.log("Quinta-feira");
-        break;
-    case "Sexta":
-        console.log("Sexta-feira");
-        break;
-    case "Sábado":
-        console.log("Sábado sem feira kkkk");
-        break;
     default:
-        console.log("Valor inexistente!");
+        console.log("Valor inexistente.");
 }
