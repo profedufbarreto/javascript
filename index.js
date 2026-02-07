07338996ede1c6b717d0e24ca68d0fdc375d34f0
profedufbarreto <01289211027@senacrs.edu.br>
2026-02-07 15:56:35 -0300

    function js

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-let idade = 18;
-let titulo = 'sim';
-
-if(idade >= 18 && titulo === "sim"){
-    console.log("Pode votar!");
-}else{
-    console.log("Não pode votar!");
+function saudacao(nome){
+    console.log("Olá " + nome + " seja bem-vindo!");
 }
+
+saudacao("Eduardo");
