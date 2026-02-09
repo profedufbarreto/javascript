1b495051687a21cbf1df80d4d3048f5e1e64eeae
profedufbarreto <01289211027@senacrs.edu.br>
2026-02-09 17:46:15 -0300

    objetos com array

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-let pessoa = {nome: "Eduardo", idade: 38, cidade: "Montenegro"};
+let pessoas = [
+    {nome: "Eduardo", idade: 38, cidade: "Monentegro"},
+    {nome: "Fernanda", idade: 46, cidade: "Montenegro"},
+    {nome: "Tânia", idade: 73, cidade: "Montenegro"}
+];
 
-console.log(pessoa.nome);
-console.log(pessoa.idade);
-console.log(pessoa.cidade);
+for(let pessoa of pessoas){
+    console.log(pessoa.nome);
+    console.log(pessoa.idade);
+    console.log(pessoa.cidade);
+}
