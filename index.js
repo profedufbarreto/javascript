224ea6f2d2dc8c3e0638826b7a07c62196cc1a74
profedufbarreto <01289211027@senacrs.edu.br>
2026-04-03 20:01:45 -0300

    variáveis de string

--- index.js
+++ index.js
@@ -1,29 +1,4 @@
-class Animal{
-    constructor(nome){
-        this.nome = nome;
-    }
-    emitirSom(){
-        console.log(`${this.nome} faz um som genérico..`);
-    }
-}
+let nome = "Eduardo";
+let sobrenome = "Barreto";
 
-class Cachorro extends Animal{
-    emitirSom(){
-        console.log(`${this.nome} diz: Au au!`);
-    }
-}
-
-class Gato extends Animal{
-    emitirSom(){
-        console.log(`${this.nome} diz: Miauuu!!`);
-    }
-}
-
-const rex = new Cachorro("Rex");
-rex.emitirSom();
-
-const luna = new Cachorro("Luna");
-luna.emitirSom();
-
-const mimi = new Gato("Mimi");
-mimi.emitirSom();
+console.log(`Meu nome é ${nome} ${sobrenome}.`);
