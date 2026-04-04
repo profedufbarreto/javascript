53716290157dff6d9d110be41ea7a02b05994a12
profedufbarreto <01289211027@senacrs.edu.br>
2026-04-04 09:13:30 -0300

    8 datatypes

--- index.js
+++ index.js
@@ -1,8 +1,54 @@
-const hora = 60;
+//string
+let color = "Yellow";
+let lastName = "Barreto";
 
-let minutos = 180;
+//number
+let length = 16;
+let weight = 7.5;
 
-let tempo = minutos/hora;
+//bigInt
+let x = 1234567890123456789012345n;
+let y = BigInt(1234567890123456789012345)
 
-console.log("Total de: ", tempo, " horas!");
- 
+//boolean
+let a = true;
+let b = false;
+
+//object
+const person = {firstName: "Eduardo", lastName};
+
+//array object
+const cars = ["Saab", "Volvo", "BMW"];
+
+//date object
+const date = new Date ("20026-04-04");
+
+//Undefined
+let c;
+let d;
+
+//Null
+let e = null;
+let f = null;
+
+//Symbol
+let g = Symbol();
+let h = Symbol();
+
+console.log(color);
+console.log(lastName);
+console.log(length);
+console.log(weight);
+console.log(x);
+console.log(y);
+console.log(a);
+console.log(b);
+console.log(person);
+console.log(cars);
+console.log(date);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
