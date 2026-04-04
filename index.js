32fc126be237dad33e4a079e245c4760ad77ff56
profedufbarreto <seu-email-do-github@exemplo.com>
2026-04-04 17:56:24 -0300

    while

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 let tabuada = 6;
-let mult = 10;
+let mult = 0;
 let resultado;
 
-while(mult >= 0){
+while(mult <= 10){
     resultado = mult * tabuada;
-    console.log(`O resultado de ${tabuada} * ${mult} é: ${resultado}.`);
-    
-    mult = mult - 1;
+    console.log(`O resultado de ${mult} * ${tabuada} é: ${resultado}.`);
+    mult = mult + 1;
 }
