32e572c3be33cd30667e42d663ad37871c690212
profedufbarreto <01289211027@senacrs.edu.br>
2026-04-04 09:28:44 -0300

    if, else if, else

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-let idade = 18;
+let idade = 38;
 
-if(idade >= 18){
-    console.log(`Você tem ${idade} e é maior de idade!`);
+if(idade <= 0){
+    console.log("Idade inválida!");
+}else if(idade <= 10){
+    console.log(`Você tem ${idade} e é uma criança!`);
+}else if(idade <= 14){
+    console.log(`Você tem ${idade} e é um pré-adolescente!`);
+}else if(idade <= 17){
+    console.log(`Você tem ${idade} e é adolescente!`);
+}
+else if(idade <= 65){
+    console.log(`Você tem ${idade} e é adulto!`);
 }else{
-    console.log(`Você tem ${idade} e é menor de idade!`);
+    console.log(`Você tem ${idade} e é sênior!`);
 }
