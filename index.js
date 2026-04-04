4875f56339a0941f4dfbb60c6ed1f3acd088c748
profedufbarreto <seu-email-do-github@exemplo.com>
2026-04-04 18:02:28 -0300

    Switch

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-let i = 0;
+let opcao = 1;
 
-while(i < 10){
-    console.log(`O número é ${i}`);
-    i++
+switch(opcao){
+    case 1:
+        console.log("Segunda-feira");
+        break;
+    case 2:
+        console.log("Terça-feira");
+        break;
+    case 3:
+        console.log("Quarta-feira");
+        break;
+    case 4:
+        console.log("Quinta-feira");
+        break;
+    case 5:
+        console.log("Sexta-feira");
+        break;
+    case 6:
+        console.log("Sábado");
+        break;
+    case 7:
+        console.log("Domingo");
+        break;
+    default:
+        console.log("Número inválido!");
 }
