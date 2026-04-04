6f45ea9769451a804048e55ea051f7472c7153e2
profedufbarreto <seu-email-do-github@exemplo.com>
2026-04-04 19:01:43 -0300

    bool

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-switch(new Date().getDay()){
-    case 4:
-    case 5:
-        console.log("Soon is is weekend");
-        break;
-    case 0:
-    case 6:
-        console.log("It is weekend.");
-        break;
-    default:
-        console.log("Looking forward to the weekend.");
+let have8ram = true;
+let have128gb = true;
+
+if(have128gb === "true" && have8ram === "true"){
+    console.log("It is possible to install Linux");
+}else{
+    console.log("Isn't possible to install Linux.");
 }
