5e61af1ce3aba511ba5f7694d0e713183a082b46
Eduardo Barreto <fbarreto.eduardo@gmail.com>
2026-08-17 21:14:05 -0300

    totalmente básico

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+function dobrar(numero){
+    return numero * 2;
+}
 
-document.getElementById("demo").innerHTML = "Olá mundo!";
-document.getElementById("demo1").innerHTML = "Meu nome é Eduardo!";
-document.getElementById("paragrafo").innerHTML = "Estou fazendo um teste com parágrafo dentro do JS.";
+console.log(dobrar(4));
 
+let ehMaior(a, b){
+    console.log(a, b);
+}
+
+console.log(ehMaior(19, 4));
