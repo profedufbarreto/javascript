b1c2ec9d79d92f269e4431e381e0c2626009273a
profefbarreto <01289211027@senacrs.edu.br>
2026-08-27 15:33:29 -0300

    brincando com switch

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function dobrar(numero){
-    return numero * 2;
-}
+const campoNum1 = document.getElementById("campoNum1");
+const campoNum2 = document.getElementById("campoNum2");
+const select = document.getElementById("select");
+const campoResultado = document.getElementById("campoResultado");
 
-console.log(dobrar(4));
+    const calcular = () => {
+        const n1 = Number(campoNum1.value);
+        const n2 = Number(campoNum2.value);
 
-let ehMaior(a, b){
-    console.log(a, b);
-}
+        const opcao = select.value;
 
-console.log(ehMaior(19, 4));
+        switch(opcao){
+            case 'soma':
+                const soma = n1 + n2;
+                campoResultado.innerText = `O resultado da soma é: ${soma}.`;
+                break;
+            case 'subtracao':
+                const subtracao = n1 - n2;
+                campoResultado.innerText = `O resultado da subtração é: ${subtracao}.`;
+                break;
+            case 'multiplicacao': 
+                const multiplicacao = n1 * n2;
+                campoResultado.innerText = `O resultado da multiplicação é: ${multiplicacao}.`;
+                break
+            case 'divisao':
+                if(n2 <= 0){
+                    campoResultado.innerText = "Não pode dividir por 0";
+                }else{
+                    const divisao = n1 / n2;
+                    campoResultado.innerText = `O resultado da divisão é: ${divisao}.`;
+                }
+                break;
+            default:
+                campoResultado.innerText = "Opção inválida!";
+                
+        }
+    }
+
+select.addEventListener("change", calcular);
+
+    
