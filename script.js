ef7c9e5fe0bb54dd0bf6c9cb37b58f39a01fd229
profefbarreto <01289211027@senacrs.edu.br>
2026-08-29 16:39:19 -0300

    brincando com ts e style

--- script.js
+++ script.js
@@ -2,6 +2,17 @@
 const campoAlterado = document.getElementById("campoAlterado");
 const campoInput = document.getElementById("campoInput");
 const btnAlterar = document.getElementById("btnAlterar");
+const btnStandart = document.getElementById("btnStandart");
+const textoInicial = campoAlterado.innerText;
+const standart = () => {
+    campoAlterado.style.color = "";
+    campoAlterado.style.backgroundColor = "";
+    campoAlterado.style.padding = "";
+    campoAlterado.style.border = "";
+    campoAlterado.style.borderRadius = "";
+    campoAlterado.innerText = textoInicial;
+    campoInput.value = "";
+};
 const alterar = () => {
     if (campoInput.value.trim() !== "") {
         campoAlterado.innerText = campoInput.value;
@@ -9,7 +20,9 @@ const alterar = () => {
     campoAlterado.style.color = "#ffffff";
     campoAlterado.style.backgroundColor = "#2b5278";
     campoAlterado.style.padding = "15px";
-    campoAlterado.style.border = "2px solid #000000";
+    campoAlterado.style.border = "5px solid #000000";
     campoAlterado.style.borderRadius = "8px";
+    campoAlterado.style.textAlign = "center";
 };
 btnAlterar.addEventListener("click", alterar);
+btnStandart.addEventListener("click", standart);
--- script.ts
+++ script.ts
@@ -1,9 +1,22 @@
 const campoAlterado = document.getElementById("campoAlterado") as HTMLHeadingElement;
 const campoInput = document.getElementById("campoInput") as HTMLInputElement;
 const btnAlterar = document.getElementById("btnAlterar") as HTMLButtonElement;
+const btnStandart = document.getElementById("btnStandart") as HTMLButtonElement;
+
+const textoInicial = campoAlterado.innerText;
+
+    const standart = (): void => {
+        campoAlterado.style.color = "";
+        campoAlterado.style.backgroundColor = "";
+        campoAlterado.style.padding = "";
+        campoAlterado.style.border = "";
+        campoAlterado.style.borderRadius = "";
+
+        campoAlterado.innerText = textoInicial;
+        campoInput.value = "";
+    };
 
     const alterar = (): void => {
-        
         if(campoInput.value.trim() !== ""){
             campoAlterado.innerText = campoInput.value;
         }
@@ -11,8 +24,10 @@ const btnAlterar = document.getElementById("btnAlterar") as HTMLButtonElement;
         campoAlterado.style.color = "#ffffff";
         campoAlterado.style.backgroundColor = "#2b5278";
         campoAlterado.style.padding = "15px";
-        campoAlterado.style.border = "2px solid #000000";
+        campoAlterado.style.border = "5px solid #000000";
         campoAlterado.style.borderRadius = "8px";
+        campoAlterado.style.textAlign = "center";
     };
 
     btnAlterar.addEventListener("click", alterar);
+    btnStandart.addEventListener("click", standart);
