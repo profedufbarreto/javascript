793b332607dd4d44885933bde8cb91b115cf637f
profefbarreto <01289211027@senacrs.edu.br>
2026-09-17 20:28:36 -0300

    Brincando com ts

--- script.js
+++ script.js
@@ -4,10 +4,13 @@ const campoCor = document.getElementById("campoCor");
 const campoSize = document.getElementById("campoSize");
 const btnFormatar = document.getElementById("btnFormatar");
 const formatar = () => {
-    campoResultado.innerText = "Esse texto mudou";
     const color = campoCor.value;
     const size = Number(campoSize.value);
+    campoResultado.innerHTML = "Esse texto <b>mudou!!</b>";
     campoResultado.style.color = `${color}`;
     campoResultado.style.fontSize = `${size}px`;
+    campoResultado.style.border = "3px solid black";
+    campoResultado.style.borderRadius = "35px";
+    campoResultado.style.textAlign = "center";
 };
 btnFormatar.addEventListener("click", formatar);
--- script.ts
+++ script.ts
@@ -5,14 +5,16 @@ const btnFormatar = document.getElementById("btnFormatar") as HTMLButtonElement;
 
     const formatar = (): void => {
 
-        campoResultado.innerText = "Esse texto mudou";
-        
         const color = campoCor.value;
         const size = Number(campoSize.value);
 
+        campoResultado.innerHTML = "Esse texto <b>mudou!!</b>";
+
         campoResultado.style.color = `${color}`;
         campoResultado.style.fontSize = `${size}px`;
-        
+        campoResultado.style.border = "3px solid black";
+        campoResultado.style.borderRadius = "35px";
+        campoResultado.style.textAlign = "center";
     }
 
     btnFormatar.addEventListener("click", formatar);
